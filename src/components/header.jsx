3429edac621e9a6d39f2e66ce49544b63e27117d
Yayo <yacoubbeltanivelle@gmail.com>
2025-03-03 16:57:30 +0100

    deuxieme

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+
+function header() {
+ 
+
+  return (
+    <header>
+        <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-
+3262834_960_720.png'/>
+
+<nav>
+    <a href="/"></a>
+</nav>
+    </header>
+  )
+}
+
+export default header
