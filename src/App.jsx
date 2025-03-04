ffca9faa947db51249a576aa8c62c1a39d936d40
Yayo <yacoubbeltanivelle@gmail.com>
2025-03-04 10:56:16 +0100

    ACTIVITE 5 : Le pied de page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from './components/Header';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <Router>
       <Header />
       <h1>Bienvenue sur la page d'accueil</h1>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+
+
+  return (
+    <footer>
+        <p>Copyright Mexican Food 2024</p>
+    </footer>
+  )
+}
+
+export default Footer;
