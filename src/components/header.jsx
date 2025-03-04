5f266491add899a4f8697e98a006cf5bb2ff2bfb
Yayo <yacoubbeltanivelle@gmail.com>
2025-03-04 10:33:52 +0100

    img-Ajoute

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,17 @@
 
-function header() {
+function Header() {
  
 
   return (
     <header>
-        <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-
-3262834_960_720.png'/>
+        <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png'/>
 
 <nav>
+    <p>Salut</p>
     <a href="/"></a>
 </nav>
     </header>
   )
 }
 
-export default header
+export default Header
