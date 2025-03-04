023542781283692b9bec17e64d18d047463352ec
Yayo <yacoubbeltanivelle@gmail.com>
2025-03-04 10:51:47 +0100

    ACTIVITE 4 : Créer votre premier composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-
-import Header from './components/header'
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from './components/Header';
 
 function App() {
-
   return (
-    <>
-
-<Header/>
-        
-      
-    </>
-  )
+    <Router>
+      <Header />
+      <h1>Bienvenue sur la page d'accueil</h1>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
 
 function Header() {
- 
+
 
   return (
     <header>
         <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png'/>
 
 <nav>
-    <p>Salut</p>
-    <a href="/"></a>
-</nav>
+      <ul>
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+      </ul>
+    </nav>
     </header>
   )
 }
 
-export default Header
+export default Header;
