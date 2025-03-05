d9be40e75b7357204415d48838ae63b910ef9590
Yayo <yacoubbeltanivelle@gmail.com>
2025-03-05 16:44:16 +0100

    ACTIVITE 13 : Filtrer les produits

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,50 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import dishes from "./components/dish/Dishes";
+import Button from "react-bootstrap/Button";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Dish from "./components/dish/Dish";
 import "./assets/App.scss";
+import { useState } from "react";
 
 function App() {
   const FilterDishes = dishes.filter((dishes) => dishes.stock > 0);
+  const [showNewOnly, setShowNewOnly] = useState(false);
+
+  const handleShowNewOnly = () => {
+    setShowNewOnly((prev) => !prev);
+  };
+
+  const filteredDishes = dishes.filter(
+    (dish) => dish.stock > 0 && (!showNewOnly || dish.isNew)
+  );
 
   return (
     <Router>
       <Header />
-      <Container className="content">
-        <div>
-          <Row>
-            {FilterDishes.map((dish, index) => (
-              <Col key={index} md={4}>
-                <Dish {...dish} />
-              </Col>
-            ))}
-          </Row>
-        </div>
-      </Container>
+      <main>
+        <Container className="content">
+          <Button
+            variant="outline-primary mb-5
+        "
+            onClick={handleShowNewOnly}
+          >
+            {showNewOnly ? "Afficher tous les plats" : "Nouveautés uniquement"}
+          </Button>
+          <div>
+            <Row>
+              {filteredDishes.map((dish, index) => (
+                <Col key={index} md={4}>
+                  <Dish {...dish} />
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
+      </main>
+
       <Footer />
     </Router>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,20 @@
-import Navigation from "../navbar/Navbar";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./Header.scss";
+import logo from "../../assets/images/logo.png";
 
 function Header() {
-  return <Navigation />;
+  return (
+    <Navbar>
+      <Container>
+        <Navbar expand="lg" fixed="top" className="bg-body-tertiary">
+          <Container bg="dark">
+            <img src={logo} alt="Logo" className="logo" />
+            <Navbar.Brand href="/">Accueil</Navbar.Brand>
+          </Container>
+        </Navbar>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
